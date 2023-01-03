9798de48b092417d67240cc70baac317b7bacce6
Pavani <pavani5.veerepalli@gmail.com>
2023-01-03 10:36:56 +0530

    Given info about how to push to remote repo

---
+++ Add.java
@@ -0,0 +1,11 @@
+Hi,
+	This is github practice repo
+	This file has to be first added 
+	and then it should be commited to
+	local repo.
+	Then we have to create a repo in remote repository
+	and then we have to make a link between local repo 
+	and remote repo.
+	Then we have to push the code to remote repo
+
+
